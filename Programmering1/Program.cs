058cbeb96bedfcb3311ea6428c57da67185a61db
John <john7272@mail.hansenberg.dk>
2023-12-05 14:48:34 +0100

    Lært datatyper - mangler bool

--- Programmering1/Program.cs
+++ Programmering1/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+
+/*Datatyper, int, double, string, char, bool, decimal, float, long, short, byte, uint, ulong, ushort, sbyte
+int = heltal  
+Float = decimaltal
+string = text
+bool = sandt eller falskt
+*/
+
+/*
+int x;
+x = 5;
+
+int y = 10, z = 15;
+
+Console.WriteLine("x is "+x);
+Console.WriteLine("y is "+y);
+Console.WriteLine("z is "+z);
+
+float d = 12.5f;
+
+Console.WriteLine(d + x - z / y);
+
+int a = Convert.ToInt16;
+
+Console.WriteLine("Float to intet "+ a);
+*/
+
+string str = "I am a string" + "and I am a string too";
+
+Console.WriteLine(str);
